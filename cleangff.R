e0c6bc3ed76d5cce63bf4540e5ea167928002071
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2017-10-11 16:33:57 -0600

    some changes to r script

--- cleangff.R
+++ cleangff.R
@@ -1,10 +1,10 @@
 #clean gff3 (changed extension from gff3 to tsv)
 require(dplyr)
 require(tidyr)
-colnames(data)<-c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes")
 data <- read.table(file = 'Pvulgaris_442_v2.1.gene.tsv', sep = '\t', header = FALSE, skip = 3)
+colnames(data)<-c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes")
 #extract genes only 
 data <- filter(data, type=="gene")
 #sort by chromosome/scaffold and by starting coordinate
 data <- data[with(data, order(seqid,start)), ]
-write.table(test, file='test.tsv', quote=FALSE, sep='\t', col.names = NA)
+write.table(data, file='genes.gff3', quote=FALSE, sep='\t', col.names = F, row.names=F)
